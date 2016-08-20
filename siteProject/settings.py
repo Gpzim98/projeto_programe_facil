22b916c74beea043686912861a607abff4851c2b
gpzim98 <gpzim98@gmail.com>
2016-08-20 19:13:42 +0100

    Moving to DEBUG False

--- siteProject/settings.py
+++ siteProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.programefacil.com.br', 'programefacil.com.br']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'srv146.prodns.com.br'
